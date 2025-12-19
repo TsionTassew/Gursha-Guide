7646b02304294c929067735552718fa1203be695
Tsion Tassew <tsiontassew91@gmail.com>
2025-12-20 01:49:07 +0300

    fontawsome installed

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './Pages/Home';
+import Recipes from './Pages/Recipes';
+import Settings from './Pages/Settings';
+import Contact from './Pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+
+      </div>
+      <Footer />
+    </Router>
+
+  );
+}
+
+export default App;
+
+
+
+
+
+
